9f557aa84228aeb5449b8d9c3a5dcceecb0edd29
Udara Wijesekara <usawijesekara@gmail.com>
2026-05-07 20:26:45 +0530

    [BE] Dependencies and schemas added

---
+++ backend/schemas/note.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class NoteCreate(BaseModel):
+    title: str
+    content: str
+
+class NoteResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+
+    # Response model for a note SQLAlchemy model returns ORM attributes
+    class Config:
+        from_attributes = True  # Enable attribute-based parsing, Read from object attributes
