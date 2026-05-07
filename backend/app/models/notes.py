d9d03c34276ab07541f4b360a7fb34f0be07c40a
Udara Wijesekara <usawijesekara@gmail.com>
2026-05-07 20:13:03 +0530

    [BE] Notes model added

---
+++ backend/app/models/notes.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+from app.db.database import Base
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=True)
+    content = Column(String, nullable=True)
