765006903f4086a3443fc942189a74d93b856f59
Udara Wijesekara <usawijesekara@gmail.com>
2026-05-10 22:39:56 +0530

    [BE]: CORS configuration added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.note_routes import router as note_router
 from app.api.auth_routes import router as auth_router
@@ -29,6 +31,17 @@ async def lifespan(app:FastAPI):
 # Create the FastAPI app
 app = FastAPI(
     lifespan=lifespan
+)
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.getenv("ALLOWED_ORIGINS")
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include the routers
